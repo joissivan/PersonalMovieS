ef5753d59483e33b807aef472afbdaf340aa2d30
Sivan <sivan.wang@askmedia.com>
2016-01-08 15:44:17 +0800

    Initial Commit

---
+++ PersonalMovieS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PersonalMovieS
+//
+//  Created by sivan on 16/1/8.
+//  Copyright © 2016年 sivan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

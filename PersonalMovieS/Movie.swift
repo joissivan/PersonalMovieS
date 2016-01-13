a4e078ba5643d87fbb5ce991c418e03ad7d47d68
Sivan <sivan.wang@askmedia.com>
2016-01-13 18:01:22 +0800

    update

--- PersonalMovieS/Movie.swift
+++ PersonalMovieS/Movie.swift
@@ -14,15 +14,13 @@ struct Movie {
     let thumbnailImageURL: String
     let largeImageURL: String
     let itemURL: String
-    let artistURL: String
     
-    init(name: String, rating: String, thumbnailImageURL: String, largeImageURL: String, itemURL: String, artistURL: String) {
-        self.title = name
+    init(title: String, rating: String, thumbnailImageURL: String, largeImageURL: String, itemURL: String) {
+        self.title = title
         self.rating = rating
         self.thumbnailImageURL = thumbnailImageURL
         self.largeImageURL = largeImageURL
         self.itemURL = itemURL
-        self.artistURL = artistURL
     }
     
     static func moviesWithJSON(results: NSArray) -> [Movie] {
@@ -34,23 +32,31 @@ struct Movie {
             
             for result in results {
                 
-                var name = result["title"] as? String
+                // title
+                var title = result["title"] as? String
                 
-                if let ratingDictionary = result["rating"] as? NSDictionary,
+                // rating
+                if var ratingDictionary = result["rating"] as? NSDictionary,
                     average = ratingDictionary["average"] as? Float {
-                        var rating = "$\(String(average))分"
+                        let rating = "\(String(average))分" ?? ""
                 }
                 
-                let thumbnailURL = result["artworkUrl60"] as? String ?? ""
-                let imageURL = result["artworkUrl100"] as? String ?? ""
-                let artistURL = result["artistViewUrl"] as? String ?? ""
-                
-                var itemURL = result["collectionViewUrl"] as? String
-                if itemURL == nil {
-                    itemURL = result["trackViewUrl"] as? String
+                // images
+                if var images = result["images"] as? NSDictionary {
+                    if var smallImageURL = images["small"] as? String {
+                        let thumbnailImageURL = smallImageURL ?? ""
+                    }
+                    if var mediumImageURL = images["medium"] as? String {
+                        let largeImageURL = mediumImageURL ?? ""
+                    }
                 }
                 
-                var newMovie = Movie(name: name!, rating: price!, thumbnailImageURL: thumbnailURL, largeImageURL: imageURL, itemURL: itemURL!, artistURL: artistURL)
+                // itmeURL
+                if var id = result["id"] as? String {
+                    var itemURL = "http://api.douban.com/v2/movie/subject/\(id)"
+                }
+                
+                var newMovie = Movie(title: title!, rating: rating, thumbnailImageURL: thumbnailImageURL, largeImageURL: largeImageURL, itemURL: itemURL!)
                 movies.append(newMovie)
             }
         }

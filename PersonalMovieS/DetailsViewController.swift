268e60303655936ddcc5a5b5bbaab5e473238631
Sivan <sivan.wang@askmedia.com>
2016-01-14 15:30:55 +0800

    Part6 2 views

---
+++ PersonalMovieS/DetailsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  DetailsViewController.swift
+//  PersonalMovieS
+//
+//  Created by sivan on 16/1/14.
+//  Copyright © 2016年 sivan. All rights reserved.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+    
+    var movie: Movie?
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
